63327c8e0cb804b4972588f280b446e87f2bd434
17aclemons <17aclemons@gmail.com>
2020-01-14 16:34:05 -0700

    started the assignment

---
+++ ISLR5.R
@@ -0,0 +1,21 @@
+####1####
+#Question 1) What is the probability that a bootstrap sample contains a given observation? Explain in precise detail, with math, in your own words.
+# there is a 1/n (with n being the number of observations) that a given observation is in a sample. Similar to rolling a dice,
+# getting a specific number is 1/6. 
+
+####2####
+#Question 2) What are the tradeoffs of using k-fold cross validation approach vs the validation set?
+
+####3####
+#Question 3) What are the tradeoffs of using k-fold cross validation approach vs leave one out cross validation?
+
+####4####
+#Question 4) Now that you're familiar with bootstrapping, walk through the process (in words) on how you would calculate the variance of a model prediction?
+
+####5####
+#Question 5) Create a model to estimate balance based on the variables from the ISLR::Default data set. Use the variables of your choosing.
+#A) Compute the standard error of the median balance.
+#B) Compute the 95% confidence interval of the standard error of your prediction
+
+
+
