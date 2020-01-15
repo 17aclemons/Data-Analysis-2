a54d3e6b54df7c9256d3bc4023a17aba9375353c
17aclemons <17aclemons@gmail.com>
2020-01-14 20:31:27 -0700

    get check with Miller

--- ISLR5.R
+++ ISLR5.R
@@ -5,13 +5,24 @@
 
 ####2####
 #Question 2) What are the tradeoffs of using k-fold cross validation approach vs the validation set?
-#
+
+#Validation estimate of the test error rate can be highly varable, depending on which observations are in the 
+#testing and training sets. The validation set error rate may overestimate the test error rate because it is training
+#on fewer observations. The validation set is simple and easy to implement. K fold can give more accurate estimates
+#for the test error rate due to only averaging a few models than averaging many. K fold is computationaly more expensive
+#depending on which value you select for k. 
+
 ####3####
 #Question 3) What are the tradeoffs of using k-fold cross validation approach vs leave one out cross validation?
 
-####4####
-#Question 4) Now that you're familiar with bootstrapping, walk through the process (in words) on how you would calculate the variance of a model prediction?
+#K fold is computationaly cheaper than LOOCV because it is running k models not n models. K fold tends to have
+#a lower variance that LOOCV due to averaging a few different models than many similar ones. 
 
+####4####
+#Question 4) Now that you're familiar with bootstrapping, 
+#walk through the process (in words) on how you would calculate the variance of a model prediction?
+
+#
 ####5####
 #Question 5) Create a model to estimate balance based on the variables from the ISLR::Default data set. Use the variables of your choosing.
 #A) Compute the standard error of the median balance.
