c98143c103410f4860b2270d50677fc225be9ca3
17aclemons <17aclemons@gmail.com>
2020-01-29 22:04:48 -0700

    start file

--- PUGB Project/PlayerPrediction.R
+++ PUGB Project/PlayerPrediction.R
@@ -6,4 +6,6 @@ test <- read.csv("test_V2.csv")
 
 part <- round((nrow(train) *.01))
 
-sample.Train <- sample(train, part)
+sample.Train <- sample(train, 1:part)
+write.csv(sample.Train, "sampleTrain.csv")
+
