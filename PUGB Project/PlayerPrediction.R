34f6a3697dfc7222d39dbeb03830000623533825
17aclemons <17aclemons@gmail.com>
2020-02-01 20:21:39 -0700

    Worked on this

--- PUGB Project/PlayerPrediction.R
+++ PUGB Project/PlayerPrediction.R
@@ -1,11 +1,34 @@
 #Andrew Clemons
 #PUBG Project
+library(caret)
+library(dplyr)
 
 train <- read.csv("train_V2.csv")
 test <- read.csv("test_V2.csv")
 
-part <- round((nrow(train) *.01))
+#### Create a sample of  entire test set ####
+set.seed(55555)
+  #remove the rows with missing win placement percents in training set
+  train <- train_V2
+  train <- train[!is.na(train$winPlacePerc),]
+  #partition the data to make it easier to work with
+  inTrain <- createDataPartition(y = train$winPlacePerc, p =.05, list = FALSE)
+  #create train and testing set
+  train_partition <- train[inTrain,]
+  test_partition <- train[-inTrain,]
+  #save for later
+  write.csv(train_partition, "train_partition.csv")
+  write.csv(test_partition, "test_partition.csv")
 
-sample.Train <- sample(train, 1:part)
-write.csv(sample.Train, "sampleTrain.csv")
-
+####Split to Solo and group games ####
+  soloTrain <- train_partition %>%
+    filter(matchType == "solo")
+  
+  groupTrain <- train_partition %>%
+    filter(matchType == "duo" |matchType == "squad")
+  
+  drop <- c("DBNOs", "matchType", "revives", "teamKills")
+  soloTrain <- soloTrain[,!names(soloTrain) %in% drop]
+  
+  lm.model <- lm(winPlacePerc ~., data = soloTrain)
+  
