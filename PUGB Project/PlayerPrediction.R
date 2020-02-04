90b67603ed89cf6992f0bac8941443ed8166da88
17aclemons <17aclemons@gmail.com>
2020-02-03 20:41:20 -0700

    stuck

--- PUGB Project/PlayerPrediction.R
+++ PUGB Project/PlayerPrediction.R
@@ -7,8 +7,8 @@ library(randomForest)
 library(gbm)
 library(data.table)
 
-train <- fread("C:/Users/17acl/OneDrive/Desktop/Software/Data-Analysis-2/PUGB Project/train_partition.csv")
-test <- read.csv("test_V2.csv")
+train <- fread("C:/Users/17acl/OneDrive/Desktop/Software/PUBG CSV's/train_V2.csv")
+test <- fread("C:/Users/17acl/OneDrive/Desktop/Software/PUBG CSV's/test_V2.csv")
 
 
 ####Data cleaning####
@@ -16,32 +16,26 @@ train <- train[!is.na(train$winPlacePerc),]
 
 ####Split to Solo and group games functions ####
 
-soloGroup <- function(train_partition) {
-  soloTrain <- train_partition %>%
-  filter(matchType == "solo" |  matchType == "normal-solo" | matchType == "normal-solo-fpp" 
+soloGroup <- function(df) {
+  soloTrain <- df %>%
+    filter(matchType == "solo" |  matchType == "normal-solo" | matchType == "normal-solo-fpp" 
          | matchType == "solo-fpp" | matchType == "flaretpp"| matchType == "flarefpp")
-  #drop group columns in solo set
-  drop <- c("DBNOs", "revives", "teamKills","X", "Id", "groupId","matchId","assists")
-  soloTrain <- soloTrain[,!names(soloTrain) %in% drop]
   return(soloTrain)
 }
 
-squadGroup <- function(train_partitioin){
-  temp <- train_partition %>%
-  filter(matchType == "duo" |matchType == "squad" | matchType == "duo-fpp" |
+squadGroup <- function(df){
+  temp <- df %>%
+    filter(matchType == "duo" |matchType == "squad" | matchType == "duo-fpp" |
          matchType == "normal-duo-fpp" |  matchType == "normal-squad" 
          | matchType == "normal-squad-fpp" | matchType == "squad-fpp" | matchType == "crashfpp"
          | matchType == "crashtpp" | matchType == "normal-duo"| matchType == "normal-duo-fpp")
-  #might not want to drop group ID
-  drop <- c("X", "Id", "groupId","matchId")
-  temp <- temp[,!names(temp) %in% drop]
   return(temp)
 }
 
 
 ####Data splitting####
 #partition the data to make it easier to work with
-inTrain <- createDataPartition(y = train$winPlacePerc, p =.01, list = FALSE)
+inTrain <- createDataPartition(y = train$winPlacePerc, p =.001, list = FALSE)
 #create train and testing set
 train_partition <- train[inTrain,]
 test_partition <- train[-inTrain,]
@@ -51,15 +45,35 @@ test_partition <- train[-inTrain,]
 #write.csv(test_partition, "test_partition.csv")
 
 ####Training model on solo####
-
-#### LM model ####
+#group the train and test partitions into only solo games and group games
 soloTrain <- soloGroup(train_partition)
+soloTest <- soloGroup(test_partition)
+
+soloID <- soloTest$Id
+drop <- c("DBNOs", "revives", "teamKills","X", "groupId","matchId","assists","roadKills","vehicleDestroys","Id")
+soloTrain <- soloTrain[,!names(soloTrain) %in% drop]
+#soloTest <- soloTest[,!names(soloTest) %in% drop]
+
+squadTrain <- squadGroup(train_partition)
+squadTest <- squadGroup(test_partition)
+
+squadID <- squadTest$Id
+drop <- c("X", "groupId","matchId","Id")
+squadTrain <- squadTrain[,!names(squadTrain) %in% drop]
+#squadTest <- squadTest[,!names(squadTrain) %in% drop]
+
+  #convert match type to factor
+  soloTrain$matchType <- as.factor(soloTrain$matchType)
+  soloTest$matchType <- as.factor(soloTest$matchType)
+  squadTrain$matchType <- as.factor(squadTrain$matchType)
+  squadTest$matchType <- as.factor(squadTest$matchType)
+  
+  
+#### LM model ####
 lm.model <- lm(winPlacePerc ~., data = soloTrain)
 
-soloTest <- soloGroup(test_partition)
 pred <- predict(lm.model, newdata = soloTest)
-
-
+mae(test_partition$winPlacePerc, pred)
 
 #### Random Forest ####
 bagging.model <- randomForest(winPlacePerc ~., data = soloTrain, mtry = 21, importance = TRUE)
@@ -67,39 +81,37 @@ bagging.model <- randomForest(winPlacePerc ~., data = soloTrain, mtry = 21, impo
 soloTest <- soloGroup(test_partition)
 pred <- predict(bagging.model, newdata = soloTest)
 
-RMSE(pred, test_partition$winPlacePerc)
+MAE(pred, test_partition$winPlacePerc)
 
 #### GBM model ####
-#solo model
-soloTrain <- soloGroup(train_partition)
-
+#Solo Model
 solo_gbm.model <- gbm(
-  formula = winPlacePerc ~.,
+  formula = winPlacePerc ~., #the model ignores the id column
   distribution = "gaussian",
   data = soloTrain,
   n.trees = 10000,
   cv.folds = 5,
-  verbose = TRUE
+  verbose = FALSE
 )
-soloTest <- soloGroup(test_partition)
-pred <- predict(gbm.model, newdata = soloTest)
 
 #optimal number of tree estimate
-ntree_opt_cv <- gbm.perf(gbm.model, method = "cv")
+ntree_opt_cv <- gbm.perf(solo_gbm.model, method = "cv")
 
-####group model####
-squadTrain <- squadGroup(train_partition)
-
+####Group Model####
 group_gbm.model <- gbm(
   formula = winPlacePerc ~.,
   distribution = "gaussian",
   data = squadTrain,
-  n.trees = 9210,
+  n.trees = 7200,
   cv.folds = 5,
-  verbose = TRUE
+  verbose = FALSE
 )
-squadTest <- squadGroup(test_partition)
-pred <- predict(gbm.model, newdata = squadTest)
 
 #optimal number of tree estimate
-ntree_opt_cv <- gbm.perf(gbm.model, method = "cv")
+ntree_opt_cv <- gbm.perf(group_gbm.model, method = "cv")
+
+soloPred <- predict(solo_gbm.model, newdata = soloTest)
+groupPred <- predict(group_gbm.model, newdata = squadTest)
+
+
+
