ab5f43b659402bdaf7c5d7ba358544bf6f4ce733
17aclemons <17aclemons@gmail.com>
2020-02-22 15:54:23 -0700

    added instructions

--- ISLR10.R
+++ ISLR10.R
@@ -1,3 +1,12 @@
 #Andrew Clemons
 #ISLR 10
 
+####1####
+#Perform principal components analysis on the data. Comment on these components. Which ones are most relevant? 
+#What are your ideas on what they mean? 
+#You have an expert code auditor on staff to provide the results of your principal components analysis. 
+#Write an email in the text box of your submission explaining the next steps on what they need to do with your results
+#and how they could be used.
+
+####2####
+#Create a cluster analysis. Use the type of cluster analysis that would be most appropriate for this type of data.
