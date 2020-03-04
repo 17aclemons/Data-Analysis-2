5b536de3ebb876589f001a7f21c0daaf022663d1
17aclemons <17aclemons@gmail.com>
2020-03-03 18:28:12 -0700

    modified variables

--- HousingProject/HousingProject.R
+++ HousingProject/HousingProject.R
@@ -4,19 +4,35 @@
 library(caret)
 library(e1071)
 library(ggplot2)
+library(bnstruct)
 
 progress = data.frame("Attempt" = c(1,2,3), "Score" = c(18.16, 18.5, 17.85))
 
 ####Import Laptop####
-#train_df <- read.csv("C:/Users/XPS/Desktop/Software/Data-Analysis-2/HousingProject/train.csv", stringsAsFactors = TRUE)
-#test_df <- read.csv("C:/Users/XPS/Desktop/Software/Data-Analysis-2/HousingProject/test.csv", stringsAsFactors = TRUE)
+train_df <- read.csv("C:/Users/XPS/Desktop/Software/Data-Analysis-2/HousingProject/train.csv", stringsAsFactors = TRUE)
+test_df <- read.csv("C:/Users/XPS/Desktop/Software/Data-Analysis-2/HousingProject/test.csv", stringsAsFactors = TRUE)
 
 ####Import Desktop####
-train_df <- read.csv("C:/Users/17acl/OneDrive/Desktop/Software/Data-Analysis-2/HousingProject/train.csv",stringsAsFactors = TRUE)
-test_df <- test <- read.csv("C:/Users/17acl/OneDrive/Desktop/Software/Data-Analysis-2/HousingProject/test.csv", stringsAsFactors = TRUE)
+#train_df <- read.csv("C:/Users/17acl/OneDrive/Desktop/Software/Data-Analysis-2/HousingProject/train.csv",stringsAsFactors = TRUE)
+#test_df <- test <- read.csv("C:/Users/17acl/OneDrive/Desktop/Software/Data-Analysis-2/HousingProject/test.csv", stringsAsFactors = TRUE)
 
 ####Functions####
-#remove missing values by calculating mean for numerics and mode for factors
+#Using the old Noggin
+#more accurate clean function for each variable
+clean_df <- function(df){
+  df$LotFrontage <- ifelse(is.na(df$LotFrontage) == TRUE, 0, df$LotFrontage)
+  df$Alley <- ifelse(is.na(df$Alley) == TRUE, as.factor("None"), df$Alley)
+  df$MasVnrType <- ifelse(is.na(df$MasVnrType) == TRUE, as.factor("None"),df$MasVnrType)
+  df$MasVnrArea <- ifelse(is.na(df$MasVnrArea) == TRUE, 0, df$MasVnrArea)
+  df$BsmtQual <- ifelse(is.na(df$BsmtQual) == TRUE, as.factor("NA"), df$BsmtQual)
+  df$BsmtCond <- ifelse(is.na(df$BsmtCond) == TRUE, as.factor("NA"), df$BsmtCond)
+  df$BsmtExposure <- ifelse(is.na(df$BsmtExposure) == TRUE, as.factor("NA"), df$BsmtExposure)
+  df$BsmtFinType1 <- ifelse(is.na(df$BsmtFinType1) == TRUE, as.factor("NA"), df$BsmtFintype1)
+  
+  return(df)
+}
+
+#remove all missing values by calculating mean for numerics and mode for factors
 clean_all_values <- function(df_column){
   if(class(df_column) == "integer" | class(df_column) == "numeric"){
     mean <- mean(df_column,na.rm = TRUE)
@@ -32,15 +48,17 @@ clean_all_values <- function(df_column){
     }
   }
 }
+
 #got mode function from https://www.tutorialspoint.com/r/r_mean_median_mode.htm
 getmode <- function(v) {
   uniqv <- unique(v)
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
+
 #run clean_values on a dataframe
 run_clean_all_values <- function(df){
   for(i in 1:ncol(df)){
-    df[,i] <- clean_values(df[,i])
+    df[,i] <- clean_all_values(df[,i])
   }
   #see which variables are na
   print(sum(is.na(df)))
@@ -50,8 +68,11 @@ run_clean_all_values <- function(df){
 #create kaggle submission
 submission <- function(test_id, pred){
   submission <- data.frame("Id" = test_id, "SalePrice" = pred)
-  #write.csv(submission, "C:/Users/XPS/Desktop/Software/Data-Analysis-2/HousingProject/submission.csv",row.names = FALSE)
-  write.csv(submission, "C:/Users/17acl/OneDrive/Desktop/Software/Data-Analysis-2/HousingProject/submission.csv", row.names = FALSE)
+  #laptop
+  write.csv(submission, "C:/Users/XPS/Desktop/Software/Data-Analysis-2/HousingProject/submission.csv",row.names = FALSE)
+  
+  #desktop
+  #write.csv(submission, "C:/Users/17acl/OneDrive/Desktop/Software/Data-Analysis-2/HousingProject/submission.csv", row.names = FALSE)
 }
 
 ####Clean Data ####
@@ -59,34 +80,27 @@ submission <- function(test_id, pred){
 train <- train_df
 test <- test_df
 
-#Using the old Noggin
-#get a list of the columns with NA's
-
-clean_df <- function(df){
-  df$MSZoning <- ifelse(is.na(df$LotFrontage) == TRUE, median(df$LotFrontage), df$LotFrontage)
-  df$Alley <- ifelse(is.na(df$Alley) == TRUE, as.factor("None"), df$Alley)
-  
-  return(df)
-}
-list <- apply(is.na(train), 2, which)
 #clean the rest of the variables
 train <- clean_df(train)
 test <- clean_df(test)
+
+#get a list of the columns with NA's
+list <- apply(is.na(train), 2, which)
 
 train <- run_clean_all_values(train)
 test <- run_clean_all_values(test)
 
 #create validation set
 inTrain <- createDataPartition(y = train$SalePrice, p = .8, list = FALSE)
-train_df <- train[inTrain,]
+train_set <- train[inTrain,]
 validation <- train[-inTrain,]
 
 ####Run Linear Regression####
-lm_model <- lm(SalePrice ~., data = train)
-lm_pred <- predict(object = lm_model, newdata = validation)
+lm_model <- lm(SalePrice ~., data = train_set)
+lm_pred <- predict(object = lm_model, newdata = test)
 RMSE(lm_pred, pred = validation$SalePrice)
 
-####Lasso and Ridge Regression
+####Lasso and Ridge Regression ####
 control <- trainControl(
   method = "cv", number = 3, verboseIter = TRUE
 )
