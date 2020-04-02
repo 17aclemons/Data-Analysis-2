53fc1a1b7e9ccf703197462b4657ae9998f2b2f6
17aclemons <17aclemons@gmail.com>
2020-04-01 19:54:18 -0600

    Finished MNist project, created RMD report

--- MNIST-Project/MNIST-Project.R
+++ MNIST-Project/MNIST-Project.R
@@ -5,7 +5,6 @@
 library(caret)
 library(tidyverse)
 library(randomForest)
-library(ggbiplot)
 library(stats)
 library(ranger)
 library(dplyr)
@@ -19,15 +18,16 @@ test <- read.csv("C:/Users/XPS/Desktop/Software/Data-Analysis-2/MNIST-Project/te
 #Laptop
 #train <- read.csv("C:/Users/17acl/OneDrive/Desktop/Software/Data-Analysis-2/MNIST-Project/train.csv")
 #test <- read.csv("C:/Users/17acl/OneDrive/Desktop/Software/Data-Analysis-2/MNIST-Project/test.csv")
-
-progress <- data.frame("Attempt" = c(1,2,3,4,5,6), 
-                       "Score" = c(.42,.68,91,.92,.11,.97), 
+#### Progress ####
+progress <- data.frame("Attempt" = c(1,2,3,4,5,6,7), 
+                       "Score" = c(.42,.68,91,.92,.11,.97,.88), 
                        "Model" = c("RandomForest",
                                    "Tuned Ranger",
                                    "the same ranger with different seed?",
                                    "Tuned Poly SVM",
                                    "Tuned Radial SVM",
-                                   "Tuned Poly on Full Training set"))
+                                   "Tuned Poly on Full Training set",
+                                   "First Neural Network"))
 ####Functions####
 submission <- function(label, pred){
   submission <- data.frame("ImageId" = label, "Label" = pred)
@@ -48,21 +48,20 @@ train.var <- train.pca$sdev^2
 pve <- train.var/sum(train.var)
 plot(pve, xlab = "Principle Component", 
      ylab = "Proportion of Variance Explained",
-     ylim = c(0,.06), type = "b") with 
+     ylim = c(0,.06), type = "b") 
 
 ####Data Cleaning####
 #scale the data ie divide by 255
 #clean the data
-train[,1:ncol(train)] <- train[2:ncol(train)]/255
+train[,2:ncol(train)] <- train[2:ncol(train)]/255
 zero_feature <- sapply(train[,2:ncol(train)], max) != 0 #could change this for columns with close to zero values
-zero_feature <- c(TRUE, zero_feature)
-train <- train[,zero_feature]
+train <- train[,c(TRUE,zero_feature)]
+train$label <- as.factor(train$label)
 
-test[,1:ncol(train)] <- train[2:ncol(train)]/255
-zero_feature <- sapply(test[,1:ncol(test)], max) != 0
-zero_feature <- c(TRUE, zero_feature)
-test <- test[,zero_feature]
+test[,1:ncol(test)] <- test[1:ncol(test)]/255
+test <- test[,c(TRUE, zero_feature)]
 num <- 1:nrow(test)
+
 set.seed(1)
 #split the data
 inTrain <- createDataPartition(train$label, p = .05, list = FALSE)
@@ -116,12 +115,12 @@ confusionMatrix(as.factor(rf.pred$predictions), as.factor(validation$label))
 
 rf.pred <- predict(opt.ranger, test)
 num <- 1:nrow(test)
-submission(num, rf.pred$predictions)
+#submission(num, rf.pred$predictions)
 
 ####SVM####
 train$label <- as.factor(train$label)
 train.poly <- svm(label~., data = train, kernel = "polynomial", cost = 10)
-train.rad <- svm(label~., data = train, kernel = "radial", cost = 10)
+#train.rad <- svm(label~., data = train, kernel = "radial", cost = 10)
 
 poly.pred <- predict(train.poly, validation)
 poly.pred <- round(poly.pred$predictions)
@@ -130,13 +129,13 @@ c <- c(.001,.01,.1,1)
 g <- c(.5,1,10)
 
 tune.poly <- tune(svm, label~., data = train, kernel = "polynomial", ranges = list(cost = c, gamma = g))
-tune.rad <- tune(svm, label~., data = train, kernel = "radial", ranges = list(cost = c, gamma = g))
+#tune.rad <- tune(svm, label~., data = train, kernel = "radial", ranges = list(cost = c, gamma = g))
 
 tune.poly$best.parameters
-tune.rad$best.parameters
+#tune.rad$best.parameters
 
 train.opt.poly <- svm(label~., data = train, kernel = "polynomial", cost = .001, gamma = .5)
-train.opt.rad <- svm(label~., data = train, kernel = "radial", cost = , gamma = .5)
+#train.opt.rad <- svm(label~., data = train, kernel = "radial", cost = , gamma = .5)
 
 tune.poly.pred <- predict(train.opt.poly, validation)
 confusionMatrix(as.factor(tune.poly.pred), as.factor(validation$label))
@@ -156,10 +155,10 @@ set.seed(1)
     targetLabel <- decodeClassLabels(train[,1])
     split <- splitForTrainingAndTest(train[,2:ncol(train)],
                                      y = targetLabel,
-                                     ratio = .9)
+                                     ratio = .1)
     
     nn1 <- mlp(x = split$inputsTrain, y = split$targetsTrain,
-               size = c(10,10), maxit = 100,
+               size = c(25,25), maxit = 500,
                inputsTest = split$inputsTest,
                targetsTest = split$targetsTest)
     
@@ -167,3 +166,13 @@ set.seed(1)
     nn1.preds.value <- max.col(nn1.pred)-1
     true_values <- max.col(split$targetsTest)-1
     caret::confusionMatrix(as.factor(nn1.preds.value), as.factor(true_values))
+    
+    #removed the first column so it matched the nodes in my trained model and it had a max of zero
+    nn1.test <- predict(nn1, test[,-1])
+    true_values <- max.col(nn1.test)-1
+    submission(num, true_values)
+
+  nnProgress <- data.frame("x" = c(15,10,10,15,20), 
+                           "y" = c(15,10,15,10,20), 
+                      "Score" = c(.93,.91,92,10,.94))
+  
